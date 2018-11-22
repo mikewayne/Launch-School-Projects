bea03ca01816f1a55f137586cd190c12529221f3
Michael Hoeft <mhpro2er@yahoo.com>
2018-11-21 23:58:29 -0800

    Adding next project

---
+++ lowmedhigh.rb
@@ -0,0 +1,17 @@
+#Low, Medium, or High?
+#Use Hash#select to iterate over numbers and return
+#a hash containing only key-value pairs where the
+#value is less than 25. Assign the returned hash to
+#a variable named low_numbers and print its value
+#using #p.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+#Expected output:
+#{:low:=>10}
+
+puts low_numbers = numbers.select { |k,v| v<25 }
